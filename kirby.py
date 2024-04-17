62bf20e509c842aac25a1379427dbc79037dbba3
Jason <jason2008.xue@outlook.com>
2024-04-17 11:42:15 -0400

    e

---
+++ kirby.py
@@ -0,0 +1,13 @@
+from Animation import SpriteSheet
+import pygame
+
+class Kirby(SpriteSheet):
+    def __init__(self, image, frames, width, height, scale, colour, running, total_frame):
+        super().__init__(image, frames, width, height, scale, colour, running, total_frame)
+
+    # def get_image(self, frame,scale):
+    #     image = pygame.Surface((self.width,self.height), pygame.SRCALPHA).convert_alpha()
+    #     image.blit(self.sheet,(0,0), ((frame*self.width),0, self.width, self.height))
+    #     image = pygame.transform.scale(image, (self.width*scale, self.height*scale))
+    #     image.set_colorkey(self.colour)
+    #     return image
