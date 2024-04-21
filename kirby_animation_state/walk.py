1a208d818f93b3d9a9971f58f3842f65c9e19b91
JasonPlayzs20 <103009819+JasonPlayzs20@users.noreply.github.com>
2024-04-21 18:40:04 -0400

    added kirby's pictures and the walk animation

---
+++ kirby_animation_state/walk.py
@@ -0,0 +1,7 @@
+import pygame
+from kirby_animation_state.kirby_animation import Kirby
+
+
+class Walk(Kirby):
+    def __init__(self, frames=None, scale=None):
+        super().__init__("kirby_animation_state/kirby_walk.png", frames, 24, 21, scale, (255, 255, 255), False, 11)
